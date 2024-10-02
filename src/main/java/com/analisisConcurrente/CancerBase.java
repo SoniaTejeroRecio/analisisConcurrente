37f4a83a5fea80d7d5535e456651dfbd48896790
mbp4 <mblanpon@myuax.com>
2024-10-02 11:49:35 +0200

    atributos base de datos

---
+++ src/main/java/com/analisisConcurrente/CancerBase.java
@@ -0,0 +1,87 @@
+package com.analisisConcurrente;
+
+public class CancerBase {
+    private int id;
+    private int clumpThickness;
+    private int uniformityOfCell;
+    private int SizeuniformityOfCellShape;
+    private int marginalAdhesion;
+    private int singleEpithelialCellSize;
+    private int blandChromatin;
+    private int normalNucleoli;
+    private int mitoses;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getClumpThickness() {
+        return clumpThickness;
+    }
+
+    public void setClumpThickness(int clumpThickness) {
+        this.clumpThickness = clumpThickness;
+    }
+
+    public int getUniformityOfCell() {
+        return uniformityOfCell;
+    }
+
+    public void setUniformityOfCell(int uniformityOfCell) {
+        this.uniformityOfCell = uniformityOfCell;
+    }
+
+    public int getSizeuniformityOfCellShape() {
+        return SizeuniformityOfCellShape;
+    }
+
+    public void setSizeuniformityOfCellShape(int sizeuniformityOfCellShape) {
+        SizeuniformityOfCellShape = sizeuniformityOfCellShape;
+    }
+
+    public int getMarginalAdhesion() {
+        return marginalAdhesion;
+    }
+
+    public void setMarginalAdhesion(int marginalAdhesion) {
+        this.marginalAdhesion = marginalAdhesion;
+    }
+
+    public int getSingleEpithelialCellSize() {
+        return singleEpithelialCellSize;
+    }
+
+    public void setSingleEpithelialCellSize(int singleEpithelialCellSize) {
+        this.singleEpithelialCellSize = singleEpithelialCellSize;
+    }
+
+    public int getBlandChromatin() {
+        return blandChromatin;
+    }
+
+    public void setBlandChromatin(int blandChromatin) {
+        this.blandChromatin = blandChromatin;
+    }
+
+    public int getNormalNucleoli() {
+        return normalNucleoli;
+    }
+
+    public void setNormalNucleoli(int normalNucleoli) {
+        this.normalNucleoli = normalNucleoli;
+    }
+
+    public int getMitoses() {
+        return mitoses;
+    }
+
+    public void setMitoses(int mitoses) {
+        this.mitoses = mitoses;
+    }
+
+
+}
